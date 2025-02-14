bc5454ff99207df0d82f1c3b2c55194eb008f819
Jose Flores <jose-carlos.flores@eviden.com>
2025-02-14 09:41:20 -0600

    project

---
+++ src/main/java/com/ecommerce/api_authorization_service/ApiAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.api_authorization_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthorizationServiceApplication.class, args);
+	}
+
+}
